6c71fda7289af4987c849dc02f895e875c413cad
agngwrnt <agungwiranata02@gmail.com>
2023-10-01 19:51:02 +0700

    stage 1 clean

--- router/router.go
+++ router/router.go
@@ -29,33 +29,5 @@ func SetupRouter() *gin.Engine {
 		authGroup.DELETE("/auth/:photoId", controllers.DeletePhoto)
 	}
 
-	//// Rute untuk foto
-	//r.POST("/photos", controllers.UploadPhoto)
-	//r.GET("/photos", controllers.GetPhotos)
-	//r.PUT("/photos/:photoId", controllers.UpdatePhoto)
-	//r.DELETE("/photos/:photoId", controllers.DeletePhoto)
-	//
-	//authGroup := r.Group("/photos")
-	//authGroup.Use(middlewares.AuthMiddleware())
-	//{
-	//	authGroup.POST("", controllers.UploadPhoto)
-	//	authGroup.GET("", controllers.GetPhotos)
-	//	authGroup.PUT("/:photoId", controllers.UpdatePhoto)
-	//	authGroup.DELETE("/:photoId", controllers.DeletePhoto)
-	//}
-
-	//// Rute Authentation
-	//r.POST("/photos", middlewares.AuthMiddleware(), controllers.UploadPhoto)
-	//r.GET("/photos", middlewares.AuthMiddleware(), controllers.GetPhotos)
-	//r.PUT("/photos/:photoId", middlewares.AuthMiddleware(), controllers.UpdatePhoto)
-	//r.DELETE("/photos/:photoId", middlewares.AuthMiddleware(), controllers.DeletePhoto)
-
-	//func UploadPhoto(c *gin.Context) {
-	//	userID, _ := c.Get("user_id")
-	//	router.POST("/upload-photo", controllers.UploadPhoto)
-	//	// Gunakan userID untuk operasi terkait pengguna yang terotentikasi
-	//	// ...
-	//}
-
 	return r
 }
